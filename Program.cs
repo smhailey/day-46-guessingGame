74a4e068afd3ad932c21d2c5fd513ddb16641f56
smhailey <smhailey@msn.com>
2019-08-12 09:37:11 -0600

    It works (mostly)

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace day_46_guessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool quit = false;
+      Random rng = new Random();
+      int randomNumber = rng.Next(1, 101);
+      Console.WriteLine($"random number = { randomNumber}");
+      while (!quit)
+      {
+        Console.WriteLine("Please type a number between 1 and 100 below:");
+        string stringGuess = Console.ReadLine();
+        Int32.TryParse(stringGuess, out int numGuess);
+        Console.WriteLine($"Your guess: { numGuess}");
+        if (numGuess == randomNumber)
+        {
+          Console.WriteLine("You win!");
+          quit = true;
+        }
+        else if (numGuess < randomNumber)
+        {
+          Console.WriteLine("Your guess was too low. Guess again:");
+        }
+        else
+        {
+          Console.WriteLine("Your guess was too high. Guess again:");
+        }
+
+      }
+    }
+  }
+}
